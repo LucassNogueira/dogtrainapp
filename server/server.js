f003ddbc77e2bb43d96513cbd30b0f0e8ac9d44b
LucassNogueira <simplylucasjason@gmail.com>
2022-02-15 09:40:57 -0600

    inital

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { seed } = require("./seed.js");
+const { SERVER_PORT } = process.env;
+const app = express();
+const {} = require("./controller.js");
+//
+//
+//
+app.use(express.json());
+app.use(cors());
+app.use(express.static("client"));
+//
+//
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+//
+app.listen(SERVER_PORT, () =>
+  console.log(`We slappin on port: ${SERVER_PORT}`)
+);
