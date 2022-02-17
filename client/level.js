6241730280afd6a1674985cf37737baef451a3fd
LucassNogueira <simplylucasjason@gmail.com>
2022-02-17 12:03:56 -0600

    got the new pop up working

--- client/level.js
+++ client/level.js
@@ -58,18 +58,103 @@ const removeShake = () => {
 ////////////////////////////////
 ////////////////////////////////
 const incLevel = () => {
-  let text = "Are you sure you want to level up?";
-  if (window.confirm(text) == false) {
-    return;
-  } else {
-    let dogId = window.location.href.substring(window.location.href.length - 1);
-    axios
-      .put(`/api/level/${dogId}`)
-      .then((res) => {
-        window.location.href = `/level${res.data.level}.html?dogId=${res.data.dogId}`;
-      })
-      .catch((err) => console.log(err));
-  }
+  let dogId = window.location.href.substring(window.location.href.length - 1);
+  axios
+    .put(`/api/level/${dogId}`)
+    .then((res) => {
+      window.location.href = `/level${res.data.level}.html?dogId=${res.data.dogId}`;
+    })
+    .catch((err) => console.log(err));
 };
 
-levelUpBtn.addEventListener("click", incLevel);
+// levelUpBtn.addEventListener("click", incLevel);
+
+const Confirm = {
+  open(options) {
+    options = Object.assign(
+      {},
+      {
+        title: "",
+        message: "",
+        okText: "OK",
+        cancelText: "Cancel",
+        onok: function () {},
+        oncancel: function () {},
+      },
+      options
+    );
+    const html = `
+        <div class="confirm">
+          <div class="confirm__window">
+            <div class="confirm__titlebar">
+              <span class="confirm__title">Are you sure you want to level up?</span>
+              <button class="confirm__close">&times;</button>
+            </div>
+            <div class="confirm__content">
+              Leveling up is a big deal, you want to make sure your pup is
+              responding to all commands previously listed with excitement and not
+              needing to be given a treat.
+            </div>
+            <div class="confirm__buttons">
+              <button
+                class="confirm__button confirm__button--ok confirm__button--fill"
+              >
+                Level up!
+              </button>
+              <button class="confirm__button confirm__button--cancel">
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+    `;
+    const template = document.createElement("template");
+    template.innerHTML = html;
+    //elements
+    const confirmEl = template.content.querySelector(".confirm");
+    const btnClose = template.content.querySelector(".confirm__close");
+    const btnOk = template.content.querySelector(".confirm__button--ok");
+    const btnCancel = template.content.querySelector(
+      ".confirm__button--cancel"
+    );
+
+    confirmEl.addEventListener("click", (e) => {
+      if (e.target === confirmEl) {
+        options.oncancel();
+        this._close(confirmEl);
+      }
+    });
+
+    btnOk.addEventListener("click", () => {
+      options.onok();
+      this._close(confirmEl);
+    });
+
+    [btnCancel, btnClose].forEach((el) => {
+      el.addEventListener("click", () => {
+        options.oncancel();
+        this._close(confirmEl);
+      });
+    });
+
+    document.body.appendChild(template.content);
+  },
+
+  _close(confirmEl) {
+    confirmEl.classList.add("confirm--close");
+    confirmEl.addEventListener("animationend", () => {
+      document.body.removeChild(confirmEl);
+    });
+  },
+};
+
+document.querySelector("#level-up").addEventListener("click", () => {
+  Confirm.open({
+    title: "Leveling up",
+    message: "Are you sure you want to level up?",
+    onok: () => {
+      incLevel();
+    },
+    oncancel: () => {},
+  });
+});
