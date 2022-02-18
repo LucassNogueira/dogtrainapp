e31a4a620d6e94049a2c84e3a5d6be9cc941d246
LucassNogueira <simplylucasjason@gmail.com>
2022-02-17 18:20:20 -0600

    testing own picture

--- client/index.js
+++ client/index.js
@@ -110,3 +110,17 @@ function openForm() {
 function closeForm() {
   document.getElementById("adddog").style.display = "none";
 }
+
+const imageInput = document.querySelector("#image_input");
+let uploadedImage;
+
+imageInput.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploadedImage = reader.result;
+    document.querySelector(
+      "#display_image"
+    ).style.backgroundImage = `url(${uploadedImage})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
