44c7d2d77bfcc0d64ecd5382a8adaac56e174af7
LucassNogueira <simplylucasjason@gmail.com>
2022-02-20 18:31:58 -0600

    all done just have to do front page button

--- client/level.js
+++ client/level.js
@@ -1,58 +1,3 @@
-const comeCmmd = document.querySelector("#come-command");
-const sitCmmd = document.querySelector("#sit-command");
-const shakeCmmd = document.querySelector("#shake-command");
-const levelUpBtn = document.querySelector("#level-up");
-const comeCnt = document.querySelector("#come-cnt");
-const sitCnt = document.querySelector("#sit-cnt");
-const shakeCnt = document.querySelector("#shake-cnt");
-
-/*comeCmmd
-sitCmmd
-shakeCmmd
-levelUpBtn
-variables for level 1*/
-const comeCommand = () => {
-  const comeCard = document.createElement("div");
-  comeCard.classList.add("come-card");
-  comeCnt.innerHTML = "";
-  comeCard.innerHTML = `<p class="come-p">For come, simply put your dog on a leash and say the command while you tug lightly on their leash. When he or she comes to you, reward them with a treat. This direction will take consecutive days of training, so be patient.</p><button onclick="removeCome()">Close</button>`;
-
-  comeCnt.appendChild(comeCard);
-};
-comeCmmd.addEventListener("click", comeCommand);
-const removeCome = () => {
-  comeCnt.innerHTML = "";
-};
-///////////////////////////////////////////////
-////////////////////////////////////
-
-////////////////////////////////
-const sitCommand = () => {
-  const sitCard = document.createElement("div");
-  sitCard.classList.add("sit-card");
-  sitCnt.innerHTML = "";
-  sitCard.innerHTML = `<p class="sit-p"> Hold a treat close to your dog's nose. Move your hand up, allowing his head to follow the treat and causing his bottom to lower. Once he's in sitting position, say “Sit,” give him the treat, and share affection.</p><button onclick="removeSit()">Close</button>`;
-
-  sitCnt.appendChild(sitCard);
-};
-sitCmmd.addEventListener("click", sitCommand);
-const removeSit = () => {
-  sitCnt.innerHTML = "";
-};
-//
-const shakeCommand = () => {
-  const shakeCard = document.createElement("div");
-  shakeCard.classList.add("shake-card");
-  shakeCnt.innerHTML = "";
-  shakeCard.innerHTML = `<p class="sit-p"> Hold a treat close to your dog's nose. Move your hand up, allowing his head to follow the treat and causing his bottom to lower. Once he's in sitting position, say “Sit,” give him the treat, and share affection.</p><button onclick="removeShake()">Close</button>`;
-
-  shakeCnt.appendChild(shakeCard);
-};
-shakeCmmd.addEventListener("click", shakeCommand);
-const removeShake = () => {
-  shakeCnt.innerHTML = "";
-};
-
 ////////////////////////////////
 ////////////////////////////////
 ////////////////////////////////
@@ -146,15 +91,6 @@ const Confirm = {
     });
   },
 };
-
-document.querySelector("#level-up").addEventListener("click", () => {
-  Confirm.open({
-    onok: () => {
-      incLevel();
-    },
-    oncancel: () => {},
-  });
-});
 
 ////
 //////countdown timer///////////////////////
@@ -253,3 +189,25 @@ class Timer {
 }
 
 new Timer(document.querySelector(".timer"));
+
+const card = document.querySelector("#card1");
+card.addEventListener("click", () => {
+  card.classList.toggle("flip");
+});
+const card2 = document.querySelector("#card2");
+card2.addEventListener("click", () => {
+  card2.classList.toggle("flip");
+});
+const card3 = document.querySelector("#card3");
+card3.addEventListener("click", () => {
+  card3.classList.toggle("flip");
+});
+
+document.querySelector("#level-up").addEventListener("click", () => {
+  Confirm.open({
+    onok: () => {
+      incLevel();
+    },
+    oncancel: () => {},
+  });
+});
